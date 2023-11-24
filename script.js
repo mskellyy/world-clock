c25c76a0c12f28c599aeed08109e25502771782c
Kelly <125195808+mskellyy@users.noreply.github.com>
2023-11-24 01:06:22 -0700

    js second interval on all cities

--- script.js
+++ script.js
@@ -6,30 +6,38 @@ function updateVanTime() {
   let vanTime = moment().tz("Canada/Pacific");
 
   vanDateElement.innerHTML = vanTime.format("MMMM Do YYYY");
-  vanTimeElement.innerHTML = vanTime.format("h:m:ss [<small>]A[</small>]");
+  vanTimeElement.innerHTML = vanTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
 updateVanTime();
 setInterval(updateVanTime, 1000);
 
 // New York Date/Time
+function updateNewYorkTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
 
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDateElement = newYorkElement.querySelector(".date");
-let newYorkTimeElement = newYorkElement.querySelector(".time");
-let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
 
-newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-newYorkTimeElement.innerHTML = newYorkTime.format(
-  "h:m:ss [<small>]A[</small>]"
-);
+updateNewYorkTime();
+setInterval(updateNewYorkTime, 1000);
 
 // Paris Date/Time
+function updateParisTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
 
-let parisElement = document.querySelector("#paris");
-let parisDateElement = parisElement.querySelector(".date");
-let parisTimeElement = parisElement.querySelector(".time");
-let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
-parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-parisTimeElement.innerHTML = parisTime.format("h:m:ss [<small>]A[</small>]");
+updateParisTime();
+setInterval(updateParisTime, 1000);
