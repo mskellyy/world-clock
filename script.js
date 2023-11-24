406f88d9f7dce07d77d7f7991ce0c89935808074
Kelly <125195808+mskellyy@users.noreply.github.com>
2023-11-24 00:47:50 -0700

    js for New York date and time

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ let vanTime = moment().tz("Canada/Pacific");
 
 vanDateElement.innerHTML = vanTime.format("MMMM Do YYYY");
 vanTimeElement.innerHTML = vanTime.format("h:m:ss [<small>]A[</small>]");
+
+// New York Date/Time
+
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment().tz("America/New_York");
+
+newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format(
+  "h:m:ss [<small>]A[</small>]"
+);
